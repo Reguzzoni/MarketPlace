f1ea0e2f21cf247d55f83c624ff28943c66268d0
rgiulio7 <100724415+rgiulio7@users.noreply.github.com>
2022-12-04 02:27:30 +0100

    modifica classe MarketDataEntity

--- sqlalchemy/sqlalchemyenv/entity/marketDataModel.py
+++ sqlalchemy/sqlalchemyenv/entity/marketDataModel.py
@@ -1,3 +1,4 @@
+"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer
 
@@ -13,3 +14,26 @@ class MarketDataEntity(Base):
     def __repr__(self):
         return "<MarketDataEntity(value={})>" \
             .format(self.value)
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, Float, String
+
+Base = declarative_base()
+
+
+# Entity mapping 1 at 1 every row of table market_data
+class MarketDataEntity(Base):
+    __tablename__='market_data_3'
+
+    Indice=Column(Integer(),primary_key=True)
+    Date_Time=Column(String())
+    Last=Column(Float())
+    Bid=Column(Float())
+    Ask=Column(Float())
+    Volume=Column(Integer())
+
+    def __repr__(self):
+        return '''<MarketDataEntity(Indice='{0}',Date_Time='{1}',Dat_Time='{2}',
+        Last='{3}',Bid='{4}',Ask='{5}'>'''.format(self.Indice,self.Date_Time,
+                                                  self.Last,self.Bid,self.Ask,self.Volume)
--- sqlalchemy/sqlalchemyenv/test.py
+++ sqlalchemy/sqlalchemyenv/test.py
@@ -1,3 +1,124 @@
+# ********************************************************************** #
+# * Sqlalchemy Project - Create DB                                     * #
+# * Insert data into DB -> postgres_python_marketplace_market_data_3   * #
+# ********************************************************************** #
+
+# ******************************************** #
+from datetime import datetime
+
+from sqlalchemy import select
+
+from entity.marketDataModel import MarketDataEntity
+from utils.DBUtils import get_session_from_db
+# ******************************************** #
+
+file_name = "crud.py"
+
+def log(log_value, method_name): (
+    print(datetime.now(), " - ", file_name, " : ", method_name, " - ", log_value)
+)
+
+
+def insert_market_data():
+    method_name = "insert_market_data"
+
+    # Connect to database and schema
+    session = get_session_from_db()
+    log("Connected to DB and got session", method_name)
+
+    # create rows to insert into DB
+    market_data_to_save = []
+    market_data_entity = []
+
+    # *-----------------------------------------------------------------------------------------------------------------*
+    # Import data
+
+    # ********************* #
+    import pandas as pd
+    import numpy as np
+    # ********************* #
+    # Import Dataset
+    file_name = r'C:\Users\rgiul\IdeaProjects\Giu_1\DATI\Output_tick\FX\6E\12\6E_tick_12-22.txt'
+
+    # Import data from txt into pandas -> numpy dataset
+    def read_file(filename):
+        df = pd.read_csv(filename)
+        #df = df[10:20]
+        df = df.reset_index(names='Index')
+        df = np.array(df)
+        return df
+
+    df = read_file(file_name)
+
+    # Create lists of attributes
+    Indice = []
+    Date_Time = []
+    Last = []
+    Bid = []
+    Ask = []
+    Volume =[]
+
+    for a in range(0,len(df)):
+        Indice.append(int(df[a,0]))
+        Date_Time.append(str(df[a,1]))
+        Last.append(float(df[a,2]))
+        Bid.append(float(df[a,3]))
+        Ask.append(float(df[a,4]))
+        Volume.append(int(df[a,5]))
+
+    # Class from list
+    for b in range(0,len(df)):
+        marekt_data_entity = MarketDataEntity(
+            Indice = Indice[b],
+            Date_Time = Date_Time[b],
+            Last = Last[b],
+            Bid = Bid[b],
+            Ask =Ask[b],
+            Volume =Volume[b]
+        )
+        market_data_to_save.append(marekt_data_entity)
+
+    # *-----------------------------------------------------------------------------------------------------------------*
+
+    # insert rows (by bulk collection) into the session
+    session.bulk_save_objects(market_data_to_save)
+    # commit the transaction to the DB
+    session.commit()
+    log("Committed to DB", method_name)
+
+    # close session
+    session.close()
+    log("Closed session", method_name)
+
+
+def select_all_market_data():
+    method_name = "select_all_market_data"
+
+    # Connect to database and schema
+    session = get_session_from_db()
+    log("Connected to DB and got session", method_name)
+
+    # get all market_data ordered by id
+    result = session.execute(select(MarketDataEntity).order_by(MarketDataEntity.Indice))
+    log("Select results from market_data_3 " + ' '.join(str(v) for v in result.scalars().all()), method_name)
+
+    # close session
+    session.close()
+    log("Closed session", method_name)
+
+if __name__ == "__main__":
+    # test insert
+    print("START insert_market_data")
+    insert_market_data()
+    print("FINISHED insert_market_data")
+
+    print("START select_all_market_data")
+    select_all_market_data()
+    print("FINISHED select_all_market_data")
+
+
+"""
+
 from datetime import datetime
 
 from sqlalchemy import select
@@ -67,3 +188,5 @@ if __name__ == "__main__":
     print("START select_all_market_data")
     select_all_market_data()
     print("FINISHED select_all_market_data")
+    
+"""
